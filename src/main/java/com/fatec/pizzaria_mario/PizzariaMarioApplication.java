5191eb5db558b466f3210d4e79128e426b99a8c9
MogiAgro <alexgeorge.semas@gmail.com>
2025-06-19 22:40:20 -0300

    Commit inicial com a nova estrutura

---
+++ src/main/java/com/fatec/pizzaria_mario/PizzariaMarioApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.pizzaria_mario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzariaMarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzariaMarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fatec/pizzaria_mario/PizzariaMarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.pizzaria_mario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzariaMarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
