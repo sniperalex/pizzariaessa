8eab7307a63e878a21e622dba2e9f30242257951
MogiAgro <alexgeorge.semas@gmail.com>
2025-06-19 23:27:16 -0300

    feat: Corrige nomes de pacotes e adiciona classes de Usuario

--- src/main/java/com/fatec/pizzaria_mario/config/SecurityConfig.java
+++ src/main/java/com/fatec/pizzaria_mario/config/SecurityConfig.java
@@ -10,6 +10,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.http.HttpMethod;
 
 @Configuration
 @EnableWebSecurity
--- src/main/java/com/fatec/pizzaria_mario/domain/Usuario.java
+++ src/main/java/com/fatec/pizzaria_mario/domain/Usuario.java
@@ -8,6 +8,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Collection;
 import java.util.HashSet;
